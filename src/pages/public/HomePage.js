98684120f68daed202cadeeb46f186b64a274c9c
Luis Alberto Perez Coello <luisalbertodev@gmail.com>
2020-07-30 14:32:31 -0500

    initial commit

---
+++ src/pages/public/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
+export class HomePage extends Component {
+	render() {
+		return (
+			<Grid>
+				<Row>
+					<Col xs={12}>
+						<p>akjfbjkasf</p>
+					</Col>
+				</Row>
+			</Grid>
+		);
+	}
+}
